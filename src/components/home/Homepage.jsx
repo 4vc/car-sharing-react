08f40651ec4c4e8eec953cd32535852d4e654034
Rostyslav <rostyslav.brahinets@gmail.com>
2023-05-19 22:31:25 +0300

    Refactor 'Homepage'

--- src/components/home/Homepage.jsx
+++ src/components/home/Homepage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import {useEffect, useState} from 'react';
 import axios from 'axios';
 import styles from './Homepage.module.css';
 import Header from '../header/Header.jsx';
@@ -27,46 +27,45 @@ const Homepage = () => {
       <Header idOfLoggedUser={1}/>
       <div className={styles.header}>AVAILABLE CARS</div>
       <div className={styles.content}>
-      <div>
-        {carList.map(car => (
-          <div key={car.id} className={styles.item}>
-            <div>
-              <p className={styles.year}>{car.year}</p>
-              <p className={styles.brand}>{car.brand}</p>
-              <p className={styles.model}>{car.model}</p>
-              <div
-              className={styles.image}
-              style={{
-                backgroundImage: `url(${car.image})`,
-              }}
-            />
-            <div className={styles.specification}>
-              <div className={styles.plate}>
-              <p>Plate:</p>
-              <p>{car.plate}</p>
+        <div>
+          {carList.map(car => (
+            <div key={car.id} className={styles.item}>
+              <div>
+                <p className={styles.year}>{car.year}</p>
+                <p className={styles.brand}>{car.brand}</p>
+                <p className={styles.model}>{car.model}</p>
+                <div
+                  className={styles.image}
+                  style={{
+                    backgroundImage: `url(${car.image})`,
+                  }}
+                />
+                <div className={styles.specification}>
+                  <div className={styles.plate}>
+                    <p>Plate:</p>
+                    <p>{car.plate}</p>
+                  </div>
+                  <div className={styles.category}>
+                    <p>Category:</p>
+                    <p>{car.idCategory}</p>
+                  </div>
+                </div>
+                <div className={styles.buy}>
+                  <div className={styles.price}>
+                    {new Intl.NumberFormat('en-US', {
+                      style: 'currency',
+                      currency: 'USD',
+                    }).format(car.price)}/<span className={styles.day}> DAY</span>
+                  </div>
+                  <Button text={'Rent Now'} onClick={handleButtonClick}/>
+                </div>
               </div>
-              <div className={styles.category}>
-              <p>Category:</p>
-              <p>{car.idCategory}</p>
-              </div>
-              </div>
-              <div className={styles.buy}>
-              <div className={styles.price}>
-                {new Intl.NumberFormat('en-US', {
-                  style: 'currency',
-                  currency: 'USD',
-                }).format(car.price)}/<span className={styles.day}> DAY</span>
-              </div>
-                <Button text={'Rent Now'} onClick={handleButtonClick}/>
-              </div>
-          </div>
-          </div>
-        ))}
-      </div>    
-    </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
-
 
 export default Homepage;

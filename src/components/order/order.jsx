23df3f0b9b5874ddcb0662f2918410011e95a331
Rostyslav <rostyslav.brahinets@gmail.com>
2023-05-19 21:10:36 +0300

    Add component 'Order'

---
+++ src/components/order/order.jsx
@@ -0,0 +1,98 @@
+import {useState} from 'react';
+import moment from 'moment';
+import styles from './Order.module.css';
+import Header from '../header/Header.jsx';
+import DateForm from '../form-date/DateForm.jsx';
+import TimeForm from '../form-time/TimeForm.jsx';
+import Car from '../cars/car/Car.jsx';
+import Button from '../button/Button.jsx';
+
+const Order = () => {
+  const [rentalDate, setRentalDate] = useState('');
+  const [rentalTime, setRentalTime] = useState('');
+  const [returnDate, setReturnDate] = useState('');
+  const [returnTime, setReturnTime] = useState('');
+  const [pricePerHour, setPricePerHour] = useState(0);
+
+  const handleRentalDateChange = (event) => {
+    const selectedRentalDate = event.target.value;
+    setRentalDate(selectedRentalDate);
+  };
+
+  const handleRentalTimeChange = (event) => {
+    const selectedRentalTime = event.target.value;
+    setRentalTime(selectedRentalTime);
+  };
+
+  const handleReturnDateChange = (event) => {
+    const selectedReturnDate = event.target.value;
+    setReturnDate(selectedReturnDate);
+  };
+
+  const handleReturnTimeChange = (event) => {
+    const selectedReturnTime = event.target.value;
+    setReturnTime(selectedReturnTime);
+  };
+
+  const handleCarPrice = (price) => {
+    setPricePerHour(price);
+  };
+
+  const handleButtonClick = () => {
+    if (getTotalRentalPrice() <= 0) {
+      return;
+    }
+
+    alert(`Order is in process!`);
+    window.location.href = '/';
+  };
+
+  const getTotalRentalPrice = () => {
+    const isDateTime = rentalDate && rentalTime && returnDate && returnTime;
+
+    if (!isDateTime) {
+      return 0;
+    }
+
+    const rentalDateTimeString = `${rentalDate}T${rentalTime}`;
+    const returnDateTimeString = `${returnDate}T${returnTime}`;
+    const rentalDateTime = moment(rentalDateTimeString, 'YYYY-MM-DDThh:mm A').toDate();
+    const returnDateTime = moment(returnDateTimeString, 'YYYY-MM-DDThh:mm A').toDate();
+
+    return pricePerHour * (returnDateTime - rentalDateTime) / 60 / 60 / 1000;
+  }
+
+  return (
+    <div>
+      <Header idOfLoggedUser={1}/>
+      <div className={styles.header}>ORDER</div>
+      <div className={styles.container}>
+        <div className={styles.container}>
+          <div>
+            <h2>Rental Info</h2>
+            <DateForm handleDateChange={handleRentalDateChange}/>
+            <TimeForm handleTimeChange={handleRentalTimeChange}/>
+          </div>
+          <div>
+            <h2>Return Info</h2>
+            <DateForm handleDateChange={handleReturnDateChange}/>
+            <TimeForm handleTimeChange={handleReturnTimeChange}/>
+          </div>
+        </div>
+        <div className={styles.car}>
+          <div className={styles.summary}>Rental Summary</div>
+          <Car id={0} onCarPrice={handleCarPrice}/>
+          <div className={styles.price}>
+            Total Rental Price: {new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',
+          }).format(getTotalRentalPrice())}
+          </div>
+          <Button text={'Proceed'} onClick={handleButtonClick}/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Order;

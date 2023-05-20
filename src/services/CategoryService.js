138e1a1baf53161975293f77a8dbaae998f2b1bf
Rostyslav <rostyslav.brahinets@gmail.com>
2023-05-20 13:06:30 +0300

    Update base url for service

--- src/services/CategoryService.js
+++ src/services/CategoryService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const CATEGORY_API_BASE_URL = 'http://localhost:8080/CATEGORIES';
+const CATEGORY_API_BASE_URL = 'http://localhost:8080/categories';
 
 class CategoryService {
   getAll() {

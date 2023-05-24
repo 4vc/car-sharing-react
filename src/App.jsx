38d8942d5e17dbc247dcc27b917c52d44d7ff67f
Rostyslav <rostyslav.brahinets@gmail.com>
2023-05-24 22:38:42 +0300

    Add page for sign up

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Homepage from './components/home/Homepage.jsx';
 import SignIn from './components/sign-in/SignIn.jsx';
+import SignUp from './components/sign-up/SignUp.jsx';
 import Order from './components/order/Order.jsx';
 import PageNotFound from './components/page-not-found/PageNotFound.jsx';
 
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Homepage/>}/>
         <Route path="/sign-in" element={<SignIn/>}/>
+        <Route path="/sign-up" element={<SignUp/>}/>
         <Route path="/order" element={<Order/>}/>
         <Route path="/*" element={<PageNotFound/>}/>
       </Routes>

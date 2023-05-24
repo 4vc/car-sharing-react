69f29f63a60fac4c6beca31df9306403223191b9
Rostyslav <rostyslav.brahinets@gmail.com>
2023-05-24 21:19:06 +0300

    Add finding logged admin

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,12 +6,15 @@ import adminService from '../../services/AdminService.js';
 const Header = () => {
   const [admin, setAdmin] = useState(null);
 
+  const adminId = Number(JSON.parse(localStorage.getItem('adminId')));
+
   useEffect(() => {
-      adminService.getById(idOfLoggedUser).then(
+    if (adminId > 0) {
+      adminService.getById(adminId).then(
         userResponse => setAdmin(userResponse.data)
       ).catch(error => console.error('Error fetching admin data:', error));
     }
-  );
+  });
 
   return (
     <div>

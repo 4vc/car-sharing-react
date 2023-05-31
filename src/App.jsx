3515004eabebc406a2d1edb05541df8572ef2472
Rostyslav <rostyslav.brahinets@gmail.com>
2023-05-31 14:22:29 +0300

    Add routes for logged admin

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,33 @@ import Homepage from './components/home/Homepage.jsx';
 import SignIn from './components/sign-in/SignIn.jsx';
 import SignUp from './components/sign-up/SignUp.jsx';
 import Order from './components/order/Order.jsx';
+import Admin from './components/admin/Admin.jsx';
 import PageNotFound from './components/page-not-found/PageNotFound.jsx';
+
+let routes = (
+  <Routes>
+    <Route path='/' element={<Homepage/>}/>
+    <Route path='/sign-in' element={<SignIn/>}/>
+    <Route path='/sign-up' element={<SignUp/>}/>
+    <Route path='/order' element={<Order/>}/>
+    <Route path='*' element={<PageNotFound/>}/>
+  </Routes>
+);
+
+if (localStorage.getItem('adminId')) {
+  routes = (
+    <Routes>
+      <Route path='/' element={<Homepage/>}/>
+      <Route path='/admin' element={<Admin/>}/>
+      <Route path='*' element={<PageNotFound/>}/>
+    </Routes>
+  );
+}
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Homepage/>}/>
-        <Route path="/sign-in" element={<SignIn/>}/>
-        <Route path="/sign-up" element={<SignUp/>}/>
-        <Route path="/order" element={<Order/>}/>
-        <Route path="/*" element={<PageNotFound/>}/>
-      </Routes>
+      {routes}
     </Router>
   );
 };

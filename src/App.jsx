915edf4df8ae7db185a7ff8cc8fea57c77976a46
Rostyslav <rostyslav.brahinets@gmail.com>
2023-06-09 15:21:59 +0300

    Rename component home

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Homepage from './components/home/Homepage.jsx';
+import Home from './components/home/Home.jsx';
 import SignIn from './components/sign-in/SignIn.jsx';
 import SignUp from './components/sign-up/SignUp.jsx';
 import Order from './components/order/Order.jsx';
@@ -12,7 +12,7 @@ import PageNotFound from './components/page-not-found/PageNotFound.jsx';
 
 let routes = (
   <Routes>
-    <Route path='/' element={<Homepage/>}/>
+    <Route path='/' element={<Home/>}/>
     <Route path='/sign-in' element={<SignIn/>}/>
     <Route path='/sign-up' element={<SignUp/>}/>
     <Route path='/order' element={<Order/>}/>
@@ -23,7 +23,7 @@ let routes = (
 if (localStorage.getItem('adminId')) {
   routes = (
     <Routes>
-      <Route path='/' element={<Homepage/>}/>
+      <Route path='/' element={<Home/>}/>
       <Route path='/admin' element={<Admin/>}/>
       <Route path='/admin/cars' element={<AdminCars/>}/>
       <Route path='/admin/cars/:id' element={<AdminCar/>}/>
